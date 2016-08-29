680ca4c6c5f1ab5e894eacfebf14087db16dce63
Lianella <alia396lady@yandex.ru>
2016-08-29 21:50:05 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ var link = document.querySelector(".search-btn");
 
 var popup = document.querySelector(".search-hotels");
 
+
 link.addEventListener("click", function(event) {
+	if (popup.classList.contains("search-hotels-show")) {
+	event.preventDefault(); 
+	popup.classList.remove("search-hotels-show");
+} 
+    else {
 	event.preventDefault();
 	popup.classList.add("search-hotels-show");
-      });
+}
+});
