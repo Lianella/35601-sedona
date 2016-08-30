2ef8a16ab16b004e457f7524d17ff6f56c038665
Lianella <alia396lady@yandex.ru>
2016-08-30 22:17:40 +0300

    JS#2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ var link = document.querySelector(".search-btn");
 
 var popup = document.querySelector(".search-hotels");
 
-
 link.addEventListener("click", function(event) {
 	if (popup.classList.contains("search-hotels-show")) {
 	event.preventDefault(); 
@@ -12,4 +11,5 @@ link.addEventListener("click", function(event) {
 	event.preventDefault();
 	popup.classList.add("search-hotels-show");
 }
+
 });
