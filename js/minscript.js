38de936c6ef0302c0327ce6fa45297c61562b0aa
Lianella <alia396lady@yandex.ru>
2016-08-31 19:45:36 +0300

    JS 3

--- js/minscript.js
+++ js/minscript.js
@@ -1,6 +1,1 @@
-var link=document.querySelector(".search-btn"),popup=document.querySelector
-(".search-
-hotels");link.addEventListener("click",function(a){popup.classList.contains
-("search-hotels-show")?(a.preventDefault(),popup.classList.remove("search-
-hotels-show")):(a.preventDefault(),popup.classList.add("search-hotels-
-show"))});
+var link=document.querySelector(".search-btn"),popup=document.querySelector(".search-hotels");link.addEventListener("click",function(a){popup.classList.contains("search-hotels-show")?(a.preventDefault(),popup.classList.remove("search-hotels-show")):(a.preventDefault(),popup.classList.add("search-hotels-show"))});
